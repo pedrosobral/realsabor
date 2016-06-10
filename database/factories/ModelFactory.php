ad35ec542efdef59093aeba539cdc0ae50860bb2
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-10 10:36:23 -0300

    chore(factory) add models factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,26 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Company::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->randomElement($array = array ('AMERICANAS', 'C&A',
+            'CENTAURO', 'CLARO', 'EXPRESSO CIDADAO', 'MARISA', 'BANCO DO BRASIL',
+            'NAGEM', 'O BOTICARIO', 'PASTELANDIA', 'RABELO', 'RIACHUELO',
+            'RICARDO ELETRO'))
+    ];
+});
+
+$factory->define(App\Customer::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'cpf' => $faker->randomElement($array = array($faker->numerify('###########'))),
+
+    ];
+});
+
+$factory->define(App\Meals::class, function (Faker\Generator $faker) {
+    return [
+        'price' => $faker->randomFloat($nbMaxDecimals = NULL, $min = 10, $max = 50)
+    ];
+});
