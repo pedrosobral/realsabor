44cbb19ec89edf1017c835c15202ac7c385a2cab
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-10 21:51:47 -0300

    update(table): add address and phone info to the customers table

--- database/migrations/2016_06_09_215529_create_customers_table.php
+++ database/migrations/2016_06_09_215529_create_customers_table.php
@@ -18,6 +18,16 @@ class CreateCustomersTable extends Migration
             $table->char('cpf', 11)->unique();
             $table->string('name');
             $table->timestamps();
+            // address
+            $table->string('address1');
+            $table->string('address2');
+            $table->string('neighborhood');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip_code');
+            // phones
+            $table->string('phone1');
+            $table->string('phone2');
 
             $table->foreign('company_id')
                   ->references('id')->on('companies')
