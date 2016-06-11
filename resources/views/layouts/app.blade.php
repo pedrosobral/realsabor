52e5870215082276e3267b724f45eaf44ac876e6
JapBastos <japbastos@gmail.com>
2016-06-11 16:21:19 -0300

    update(app): Add Companies Menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,6 +52,7 @@
                 <ul class="nav navbar-nav">
                     <li><a href="{{ url('/home') }}"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home</a></li>
                     <li><a href="{{ url('/customers') }}">Clientes</a></li>
+                    <li><a href="{{ url('/companies') }}">Empresas</a></li>
                     <li><a href="{{ url('/transactions') }}">Lançamentos</a></li>
                     <li><a href="{{ url('/searchs') }}">Consultas</a></li>
                     <li><a href="{{ url('/reports') }}">Relatórios</a></li>
