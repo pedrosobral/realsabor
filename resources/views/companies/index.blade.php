1e11408b0929beb92bac8776ad96612283cc348c
JapBastos <japbastos@gmail.com>
2016-06-11 18:14:03 -0300

    update(companies): panel index

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -1,8 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Empresas</h1>
-    @foreach($companies as $company)
-        <h1>{{ $company->name }}</h1>
-    @endforeach
+  <div class="container">
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1">
+        <div class="panel panel-default">
+          <div class="panel-heading">Empresas</div>
+          <div class="panel-body">
+            @foreach($companies as $company)
+                <h4>{{ $company->name }}</h4>
+            @endforeach
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
