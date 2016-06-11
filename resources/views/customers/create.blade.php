680aafa7f572a4301377624951459281d9852636
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-10 23:27:10 -0300

    update(form): add more fields, add POST route

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -5,26 +5,50 @@
     <div class="row">
       <div class="col-md-10 col-md-offset-1">
         <h2>Novo Cliente</h2>
-        {!! Form::open() !!}
+        {!! Form::open(['route' => 'customers.store']) !!}
           <div class="form-group">
             {!! Form::label('name', 'Nome:', ['for' => 'name']) !!}
             {!! Form::text('name', null, ['class' => 'form-control'])!!}
+          </div>
+          <div class="form-group">
+            {!! Form::label('cpf', 'CPF:') !!}
+            {!! Form::text('cpf', null, ['class' => 'form-control'])!!}
           </div>
           <div class="form-group">
             {!! Form::label('company', 'Empresa:') !!}
             {!! Form::text('company', null, ['class' => 'form-control'])!!}
           </div>
           <div class="form-group">
-            {!! Form::label('adress', 'Endereço:') !!}
-            {!! Form::text('adress', null, ['class' => 'form-control'])!!}
+            {!! Form::label('address1', 'Endereço:') !!}
+            {!! Form::text('address1', null, ['class' => 'form-control'])!!}
           </div>
           <div class="form-group">
-            {!! Form::label('phone', 'Telefone:') !!}
-            {!! Form::text('phone', null, ['class' => 'form-control'])!!}
+            {!! Form::label('address2', 'Complemento:') !!}
+            {!! Form::text('address2', null, ['class' => 'form-control'])!!}
           </div>
           <div class="form-group">
-            {!! Form::label('cpf', 'C.P.F.:') !!}
-            {!! Form::text('cpf', null, ['class' => 'form-control'])!!}
+            {!! Form::label('neighborhood', 'Bairro:') !!}
+            {!! Form::text('neighborhood', null, ['class' => 'form-control'])!!}
+          </div>
+          <div class="form-group">
+            {!! Form::label('city', 'Cidade:') !!}
+            {!! Form::text('city', null, ['class' => 'form-control'])!!}
+          </div>
+          <div class="form-group">
+            {!! Form::label('state', 'Estado:') !!}
+            {!! Form::text('state', null, ['class' => 'form-control'])!!}
+          </div>
+          <div class="form-group">
+            {!! Form::label('zip_code', 'CEP:') !!}
+            {!! Form::text('zip_code', null, ['class' => 'form-control'])!!}
+          </div>
+          <div class="form-group">
+            {!! Form::label('phone1', 'Telefone:') !!}
+            {!! Form::text('phone1', null, ['class' => 'form-control'])!!}
+          </div>
+          <div class="form-group">
+            {!! Form::label('phone2', 'Celular:') !!}
+            {!! Form::text('phone2', null, ['class' => 'form-control'])!!}
           </div>
           <div class="form-group">
             {!! Form::submit('Salvar', ['class' => 'btn btn-primary']) !!}
