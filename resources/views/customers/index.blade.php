9af81d0812bd3b87b9c7f7432508f47405c5628b
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-11 22:18:22 -0300

    update(button): add button to create a customer

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -1,18 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1">
-        <div class="panel panel-default">
-          <div class="panel-heading">Clientes</div>
-          <div class="panel-body">
-            @foreach($customers as $customer)
-              <h4>{{ $customer->name }}</h4>
-            @endforeach
-            {!! Form::submit('Novo Cliente', ['class' => 'btn btn-primary']) !!}
-          </div>
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Clientes</div>
+                    <div class="panel-body">
+                        <a href="{!! route('customers.create') !!}" class="btn btn-primary pull-right" role="button">
+                            <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                            Novo Cliente
+                        </a>
+                        @foreach($customers as $customer)
+                            <h4>{{ $customer->name }}</h4>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 @endsection
