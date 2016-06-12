248d3d501041f7fcdac972b1131068f7355d8d93
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-12 14:14:13 -0300

    update(dashboard): add link to pages, add companies count

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Customer;
+use App\Company;
 
 class HomeController extends Controller
 {
@@ -24,7 +25,8 @@ class HomeController extends Controller
     public function index()
     {
         $customers = Customer::all();
+        $companies = Company::all();
 
-        return view('home', compact('customers'));
+        return view('home', compact('customers', 'companies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,22 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-3 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
+                <div class="panel-heading">Resumo</div>
                 <div class="panel-body">
-                    <h3>Clientes: <span class="label label-primary">{{$customers->count()}}</span></h3>
+                    <div class="list-group">
+                        <a href="{{route('customers.index')}}" class="list-group-item">
+                            <span class="badge">{{$customers->count()}}</span>
+                            Clientes
+                        </a>
+                        <a href="{{route('companies.index')}}" class="list-group-item">
+                            <span class="badge">{{$companies->count()}}</span>
+                            Empresas
+                        </a>
+                    </div>
                 </div>
+
             </div>
         </div>
     </div>
