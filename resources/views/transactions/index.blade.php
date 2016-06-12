01fc383e65ec6388b9364df2254f982b3c69a69a
JapBastos <japbastos@gmail.com>
2016-06-12 15:04:45 -0300

    feat(transactions): Add transations index

---
+++ resources/views/transactions/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Lançamentos</div>
+                    <div class="panel-body">
+                      <div class="col-md-12">
+                        <div class="col-md-6">
+                          <a class="btn btn-primary pull-rigth" role="button">
+                              <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>
+                              Consumo
+                          </a>
+                        </div>
+                        <div class="col-md-6">
+                          <a class="btn btn-primary pull-left" role="button">
+                              <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                              Pagamento
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
