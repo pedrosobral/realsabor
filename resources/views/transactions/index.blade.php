cb6e7473b4c0c41b5f18d03f4274807f0150c551
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-14 23:45:54 -0300

    feat(new meal): add form to add new meal

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -1,60 +1,99 @@
-@extends('layouts.app') @section('content')
-<div class="container">
-  <div class="row">
-    <div class="col-md-4 col-md-offset-1">
-      <div class="panel panel-default">
-        <div class="panel-heading">Lançamentos</div>
-        <div class="panel-body">
-          <div class="col-md-12">
-            <div class="col-md-6">
-              <a class="btn btn-primary btn-block" data-toggle="modal" role="button">
-                <span class="glyphicon glyphicon-minus" aria-hidden="true"></span> Consumo
-              </a>
+<div class="modal fade" id="pagamento" role="dialog" aria-labelledby="exampleModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="exampleModalLabel">Novo Pagamento</h4>
             </div>
-            <div class="col-md-6">
-              <a class="btn btn-primary btn-block" data-toggle="modal" data-target="#pagamento" role="button">
-                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Pagamento
-              </a>
+            <div class="modal-body">
+                {!! Form::open([ 'id' => 'payment']) !!}
+                <div class="row">
+                    <input id="_id" type="hidden" value="{{$customer->id}}">
+                    <div class="col-md-8">
+                        <div class="form-group">
+                            {!! Form::label('name', 'Nome', ['for' => 'name']) !!}
+                            {!! Form::text('name', $customer->name, ['class' => 'form-control', 'placeholder' => 'Nome completo', 'disabled'])!!}
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            {!! Form::label('cpf', 'CPF', ['for' => 'cpf']) !!}
+                            {!! Form::text('cpf', $customer->cpf, ['class' => 'form-control', 'disabled'])!!}
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            {!! Form::label('date', 'Data', ['for' => 'data']) !!}
+                            <div class="input-group">
+                                <div class="input-group-addon">
+                                    <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>
+                                </div>
+                                {!! Form::date('date',  \Carbon\Carbon::now(), ['class' => 'form-control'])!!}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            {!! Form::label('price', 'Valor', ['for' => 'price']) !!}
+                            <div class="input-group">
+                                <div class="input-group-addon">R$</div>
+                                {!! Form::number('price', null, ['class' => 'form-control', 'autofocus']) !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                    {!! Form::submit('Salvar', ['class' => 'btn btn-primary', 'id' => 'submit']) !!}
+                </div>
+                {!! Form::close() !!}
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
 </div>
-<div id="pagamento" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="pag" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">
-          <span aria-hidden="true">&times;</span>
-          <span class="sr-only">Close</span>
-        </button>
-        <h4 class="modal-title" id="pag">Pagamento</h4>
-      </div>
-      <div class="modal-body">
-        <form class="form-horizontal" role="form">
-          <div class="form-group">
-            <label class="col-sm-2 control-label" for="inputEmail3">Cliente</label>
-            <div class="col-sm-10">
-              <input type="email" class="form-control" id="inputEmail3" placeholder="Email" />
-            </div>
-          </div>
-          <div class="form-group">
-            <label class="col-sm-2 control-label" for="inputPassword3">Valor</label>
-            <div class="col-sm-10">
-              <input type="password" class="form-control" id="inputPassword3" placeholder="Password" />
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-              <button type="submit" class="btn btn-default">Cancel</button>
-              <button type="submit" class="btn btn-default">Sign in</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
-@endsection
+
+<script>
+    Mousetrap.bind('1', function() {
+        $('#pagamento').modal('show');
+    });
+
+    $('#payment').submit(function(e) {
+        $('#pagamento').modal('toggle');
+        $('#pagamento').modal('hide');
+
+        e.preventDefault();
+        var id     = $('#_id').val();
+        var date  = $('#date').val();
+        var price = $('#price').val();
+
+        $.ajax({
+            type: 'POST',
+            url : "{{ route('customer.payment') }}",
+            data: {
+                id: id, date: date, price: price,
+                _token: '{{ Session::token() }}'
+            },
+            success: function(data){
+                refresh(id);
+            },
+            error: function(){
+                console.error('transactions.index.blade.php');
+            }
+        });
+    });
+
+    function refresh(id) {
+        console.log('refresh');
+        $.ajax({
+            url: 'customer/' + id,
+            success: function(data){
+                $('.details').html(data);
+            },
+            error: function(){
+                console.error('customer.index.blade.php');
+            },
+        });
+    }
+</script>
