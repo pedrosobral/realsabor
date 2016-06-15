bb0d49fa5f2a78432a08f48d2625f26d91b6fb4d
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-15 01:02:03 -0300

    upate(style): add some panels

--- resources/views/customer/show.blade.php
+++ resources/views/customer/show.blade.php
@@ -1,7 +1,7 @@
 <div class="page-header">
     <div class="btn-toolbar">
         <span class="headline">{{$customer->name}}</span>
-        <button id="buceta" data-toggle="modal" data-target="#pagamento" class="btn btn-success pull-right " role="button">
+        <button data-toggle="modal" data-target="#pagamento" class="btn btn-success pull-right " role="button">
             <span class="glyphicon glyphicon-cutlery"></span> Consumo
         </button>
         <button data-toggle="modal" data-target="#pagamento" class="btn btn-warning pull-right" role="button">
@@ -9,25 +9,51 @@
         </button>
     </div>
 </div>
-<div class="panel panel-default">
-    <div class="panel-body">
-        <h2>CPF: {{ $customer->cpf }}</h2>
+
+<div class="row">
+    <div class="col-md-6">
+        <div class="panel panel-success">
+            <div class="panel-heading">Últimas refeições</div>
+            <table id="meals" class="table table-striped"  cellspacing="0">
+                <thead>
+                    <th> Data </th>
+                    <th> Valor (R$) </th>
+                </thead>
+                <tbody>
+                    @foreach($customer->meals()->limit(5)->get() as $meal)
+                        <tr>
+                            <td> {{ $meal->date  }} </td>
+                            <td> {{ $meal->price }} </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-    <h1>Refeiçoes</h1>
-    <table id="meals" class="table table-striped"  cellspacing="0" width="100%">
-        <thead>
-            <th> Data </th>
-            <th> Valor (R$) </th>
-        </thead>
-        <tbody>
-            @foreach($customer->meals as $meal)
-                <tr>
-                    <td> {{ $meal->date  }} </td>
-                    <td> {{ $meal->price }} </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="col-md-6">
+        <div class="panel panel-warning">
+            <div class="panel-heading">Últimos Pagamentos</div>
+            <table id="meals" class="table table-striped"  cellspacing="0" width="100%">
+                <thead>
+                    <th> Data </th>
+                    <th> Valor (R$) </th>
+                </thead>
+                <tbody>
+                    @foreach($customer->meals()->limit(5)->get() as $meal)
+                        <tr>
+                            <td> {{ $meal->date  }} </td>
+                            <td> {{ $meal->price }} </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+    </div>
 </div>
+
+
+
+
 
 @include('transactions.index');
