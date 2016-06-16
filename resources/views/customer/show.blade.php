11bf6de59dc9724350a9e0d36c871341b27faa0d
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-16 13:32:12 -0300

    update(details): updapte the show details html

--- resources/views/customer/show.blade.php
+++ resources/views/customer/show.blade.php
@@ -8,18 +8,23 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-md-6">
+        <div class="well">
+                <span class="label label-primary balance">Saldo: R$ {{$balance}}</span>
+        </div>
+    <div class="col-md-5">
         <div class="panel panel-success">
             <div class="panel-heading">Últimas refeições</div>
-            <table id="meals" class="table table-striped"  cellspacing="0">
+            <table id="meals" class="table table-striped table-condensed"  cellspacing="0">
                 <thead>
+                    <th> #ID </th>
                     <th> Data </th>
                     <th> Valor (R$) </th>
                 </thead>
                 <tbody>
-                    @foreach($customer->meals()->limit(5)->get() as $meal)
+                    @foreach($customer->meals()->orderBy('created_at', 'desc')->limit(5)->get() as $meal)
                         <tr>
-                            <td> {{ $meal->date  }} </td>
+                            <td> {{ $meal->id }} </td>
+                            <td><span class="data" data-date="{{$meal->created_at}}">{{ $meal->date  }}</span></td>
                             <td> {{ $meal->price }} </td>
                         </tr>
                     @endforeach
@@ -27,19 +32,23 @@
             </table>
         </div>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-7">
         <div class="panel panel-warning">
             <div class="panel-heading">Últimos Pagamentos</div>
-            <table id="meals" class="table table-striped"  cellspacing="0" width="100%">
+            <table id="meals" class="table table-striped table-condensed"  cellspacing="0" width="100%">
                 <thead>
                     <th> Data </th>
                     <th> Valor (R$) </th>
+                    <th> Saldo (R$) </th>
+                    <th> #Consumo </th>
                 </thead>
                 <tbody>
-                    @foreach($customer->meals()->limit(5)->get() as $meal)
+                    @foreach($customer->payments()->orderBy('created_at', 'desc')->limit(5)->get() as $payment)
                         <tr>
-                            <td> {{ $meal->date  }} </td>
-                            <td> {{ $meal->price }} </td>
+                            <td><span class="data" data-date="{{$payment->created_at}}"></span> </td>
+                            <td> {{ $payment->value }} </td>
+                            <td> {{ $payment->balance }} </td>
+                            <td> {{ $payment->last_meal_id }} </td>
                         </tr>
                     @endforeach
                 </tbody>
@@ -49,4 +58,17 @@
     </div>
 </div>
 
+<script type="text/javascript">
+$(function() {
+    var then = $('.data');
+    console.log(then);
+    for (i = 0; i < then.length; i++) {
+        console.log(then[i].dataset.date);
+        var date = moment(new Date(then[i].dataset.date));
+        then[i].innerHTML = date.calendar();
+    }
+});
+
+</script>
 @include('transactions.index')
+@include('transactions.meal')
