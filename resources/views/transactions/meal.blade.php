24b94097ecf5a40c2ab76c2a5cb5bb67d43fee30
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-16 13:30:10 -0300

    chore(update): udpdate ids and add step to number input

--- resources/views/transactions/meal.blade.php
+++ resources/views/transactions/meal.blade.php
@@ -1,12 +1,12 @@
-<div class="modal fade" id="meal_modal" role="dialog" aria-labelledby="exampleModalLabel">
+<div class="modal fade" id="payment_modal" role="dialog">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Novo Consumo</h4>
+                <h4 class="modal-title">Novo Pagamento</h4>
             </div>
             <div class="modal-body">
-                {!! Form::open([ 'id' => 'meal_form']) !!}
+                {!! Form::open([ 'id' => 'payment_form']) !!}
                 <div class="row">
                     <input id="_id" type="hidden" value="{{$customer->id}}">
                     <div class="col-md-8">
@@ -36,17 +36,17 @@
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
-                            {!! Form::label('price', 'Valor', ['for' => 'price']) !!}
+                            {!! Form::label('value', 'Valor', ['for' => 'price']) !!}
                             <div class="input-group">
                                 <div class="input-group-addon">R$</div>
-                                {!! Form::number('price', null, ['class' => 'form-control', 'autofocus', 'step' => 'any']) !!}
+                                {!! Form::number('value', null, ['class' => 'form-control', 'autofocus', 'step' => 'any']) !!}
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-                    {!! Form::submit('Salvar', ['class' => 'btn btn-primary', 'id' => 'submit']) !!}
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar Pagamento</button>
+                    {!! Form::submit('Salvar Pagamento', ['class' => 'btn btn-primary', 'id' => 'submit']) !!}
                 </div>
                 {!! Form::close() !!}
             </div>
@@ -55,24 +55,24 @@
 </div>
 
 <script>
-    $('#meal_form').submit(function(e) {
-        $('#meal_modal').modal('toggle');
-        $('#meal_modal').modal('hide');
-        console.log('NOVO CONSUMO');
+    $('#payment_form').submit(function(e) {
+        $('#payment_modal').modal('toggle');
+        $('#payment_modal').modal('hide');
+        console.log('NOVO PAGAMENTO');
 
         e.preventDefault();
-        var id     = $('#_id').val();
+        var id    = $('#_id').val();
         var date  = $('#date').val();
-        var price = $('#price').val();
+        var value = $('#value').val();
         console.log(id);
         console.log(date);
-        console.log(price);
+        console.log(value);
 
         $.ajax({
             type: 'POST',
-            url : "{{ route('customer.meal') }}",
+            url : "{{ route('customer.payment') }}",
             data: {
-                id: id, date: date, price: price,
+                id: id, date: date, value: value,
                 _token: '{{ Session::token() }}'
             },
             success: function(data){
