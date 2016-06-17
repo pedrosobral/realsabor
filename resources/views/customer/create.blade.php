e8e980b3df8790e0c3b1f5a49b4dbfd13dcb8c4c
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-16 22:57:44 -0300

    update(): applying class input and showing toast errors

--- resources/views/customer/create.blade.php
+++ resources/views/customer/create.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
+
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
                     <div class="panel-heading">Novo Cliente</div>
@@ -12,33 +13,33 @@
                             <div class="col-md-8">
                                 <div class="form-group">
                                     {!! Form::label('name', 'Nome', ['for' => 'name']) !!}
-                                    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Nome completo'])!!}
+                                    {!! Form::text('name', null, ['class' => 'form-control input', 'placeholder' => 'Nome completo'])!!}
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
                                     {!! Form::label('cpf', 'CPF') !!}
-                                    {!! Form::text('cpf', null, ['class' => 'form-control'])!!}
+                                    {!! Form::number('cpf', null, ['class' => 'form-control'])!!}
                                 </div>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-md-4">
                                 <div class="form-group">
-                                    {!! Form::label('company', 'Empresa') !!}
-                                    {!! Form::select('company_id', $companies, null,['id' => 'companies_list', 'class' => 'form-control'])!!}
+                                {!! Form::label('company', 'Empresa') !!}
+                                {!! Form::select('company_id', $companies, null,['id' => 'companies_list', 'class' => 'form-control'])!!}
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
                                     {!! Form::label('phone1', 'Telefone') !!}
-                                    {!! Form::text('phone1', null, ['class' => 'form-control'])!!}
+                                    {!! Form::number('phone1', null, ['class' => 'form-control'])!!}
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
                                     {!! Form::label('phone2', 'Celular') !!}
-                                    {!! Form::text('phone2', null, ['class' => 'form-control'])!!}
+                                    {!! Form::number('phone2', null, ['class' => 'form-control'])!!}
                                 </div>
                             </div>
                         </div>
@@ -46,19 +47,19 @@
                             <div class="col-md-4">
                                 <div class="form-group">
                                     {!! Form::label('address1', 'Endereço') !!}
-                                    {!! Form::text('address1', null, ['class' => 'form-control'])!!}
+                                    {!! Form::text('address1', null, ['class' => 'form-control input'])!!}
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
                                     {!! Form::label('address2', 'Complemento') !!}
-                                    {!! Form::text('address2', null, ['class' => 'form-control'])!!}
+                                    {!! Form::text('address2', null, ['class' => 'form-control input'])!!}
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
                                     {!! Form::label('neighborhood', 'Bairro') !!}
-                                    {!! Form::text('neighborhood', null, ['class' => 'form-control'])!!}
+                                    {!! Form::text('neighborhood', null, ['class' => 'form-control input'])!!}
                                 </div>
                             </div>
                         </div>
@@ -66,19 +67,19 @@
                             <div class="col-md-4">
                                 <div class="form-group">
                                     {!! Form::label('city', 'Cidade') !!}
-                                    {!! Form::text('city', null, ['class' => 'form-control'])!!}
+                                    {!! Form::text('city', null, ['class' => 'form-control input'])!!}
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
                                     {!! Form::label('state', 'Estado') !!}
-                                    {!! Form::text('state', null, ['class' => 'form-control'])!!}
+                                    {!! Form::text('state', null, ['class' => 'form-control input'])!!}
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
                                     {!! Form::label('zip_code', 'CEP') !!}
-                                    {!! Form::text('zip_code', null, ['class' => 'form-control'])!!}
+                                    {!! Form::number('zip_code', null, ['class' => 'form-control'])!!}
                                 </div>
                             </div>
                         </div>
@@ -95,7 +96,15 @@
     </div>
 @endsection
 
+
 @section('footer')
+    @if (count($errors) > 0)
+    <script type="text/javascript">
+        @foreach ($errors->all() as $error)
+            toastr.error('{{$error}}');
+        @endforeach
+    </script>
+    @endif
     <script>
     $('#companies_list').select2({
         placeholder: "Selecione uma empresa",
