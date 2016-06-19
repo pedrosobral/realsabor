f2728ab5c187209921bc4c80a9f0a181937cb5b3
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-19 01:20:58 -0300

    feat(form): form to update customer

---
+++ resources/views/customer/edit.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Novo Cliente</div>
+                <div class="panel-body">
+                    {!! Form::model($customer, ['method' => 'PATCH',
+                            'route' => ['customer.update', $customer->id]]) !!}
+                    @include('customer._form')
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
+@section('footer')
+    @if (count($errors) > 0)
+    <script type="text/javascript">
+        @foreach ($errors->all() as $error)
+            toastr.error('{{$error}}');
+        @endforeach
+    </script>
+    @endif
+    <script>
+    $('#companies_list').select2({
+        placeholder: "Selecione uma empresa",
+        allowClear: true,
+        tags: true
+    });
+    </script>
+@endsection
