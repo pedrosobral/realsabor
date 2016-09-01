e246a2c8c2947668767db12ba05064b7aa64d7b6
Pedro Henrique <pedrosobralxv@gmail.com>
2016-09-01 17:37:19 -0300

    feat(show.companies): Mostra soma total do saldo de todos os funcionarios

--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -1,6 +1,22 @@
 <div class="page-header">
     <h1>{{$company->name}}</h1>
 </div>
+
+<div class="row">
+    <div class="col-lg-5 col-md-6 col-sm-6 col-xs-12">
+        <div class="panel panel-success panel-saldo">
+            <div class="panel-heading">
+                <h4 class="panel-title">Saldo</h4>
+            </div>
+            <div class="panel-body">
+                <div class="stats">
+                    <div class="stats-number">R$ {{number_format(floatval($company->getTotalCustomersBalance()), 2, ',', '.')}}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="panel panel-default">
     <div class="panel-body">
         <table class="datatables table table-striped">
